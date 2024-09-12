8620bd1b68130f7c4c7e7483d5876b0f25b71363
xlLotus <00001108492356sp@al.educacao.sp.gov.br>
2024-09-12 12:09:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+// fantasia, aventura, drama
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+// depois da chuva, 10, drama
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+// As Aventuras de Pi, 10, drama, fantasia, aventura.
+
+// Ladrões de bicicleta, 12, drama.
+
+
+function setup() {
+    createCanvas(800, 400);
+    createSpan("Sua idade:");
+    campoIdade = createInput("");
+    campoFantasia = createCheckbox(" Gosta de fantasia?");
+}
+function draw() {
+    background("white");
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+
+    fill(color(76, 0, 115));
+    textAlign(CENTER, CENTER);
+    textSize(38);
+    text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if (idade >= 10) {
+        if (idade >= 14) {
+            return "O menino que descobriu o vento";
+        } else {
+            if (gostaDeFantasia) {
+                return "As aventuras de pi";
+            } else {
+                return "Depois da chuva";
+            }
+        }
+    } else {
+        if (gostaDeFantasia) {
+            return "A viagem de chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
+}
